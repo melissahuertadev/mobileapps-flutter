0743a42c253dea384089b021a20b8b7dbb9ea8b5
piratelicorne <thecodeloops@gmail.com>
2020-08-16 05:49:06 -0500

    setting up imrich

--- i_am_poor/README.md
+++
@@ -1,3 +0,0 @@
-# i_am_poor
-
-i_am_poor Flutter application, this exercise is to replicate the project 'i_am_rich'
--- i_am_rich/android/app/src/main/kotlin/thecodeloops/i_am_rich/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package thecodeloops.i_am_rich
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
