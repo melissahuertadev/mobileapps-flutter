ea0cf9bc616590f407e4c99ab8eb049c7475cef9
piratelicorne <thecodeloops@gmail.com>
2020-08-19 03:02:10 -0500

    app with navigation 3 screens sample

---
+++ nav_sample/android/app/src/main/kotlin/com/example/nav_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nav_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ nav_sample/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:nav_sample/screen2.dart';
+import 'screen0.dart';
+import 'screen1.dart';
+import 'screen2.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      //home: Screen0(),
+      routes: {
+        '/': (context) => Screen0(),
+        '/first': (context) => Screen1(),
+        '/second': (context) => Screen2(),
+      },
+    );
+  }
+}
